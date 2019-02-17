252b8f0c28a5456c515ada8727e0220e31b77b9d
rkanadam <raghuram.kanadam@gmail.com>
2019-02-17 01:04:06 -0800

    Fixing hapijs auth schemes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,38 @@
-import { Server } from 'hapi';
+import { Request, ResponseToolkit, Server } from 'hapi';
+import Boom = require('boom');
+
+const CLIENT_ID = '172433576772-crg5vtfavpqsbn4mitk7ffl2s73o93fc.apps.googleusercontent.com';
 
 export const server: Server = new Server({
     port: 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
 });
+
+server.auth.scheme('google', (server: Server) => {
+    return {
+        async authenticate(request: Request, h: ResponseToolkit) {
+            const authorization = request.headers.authorization;
+            if (!authorization) {
+                throw Boom.unauthorized();
+            }
+            const payload = await verify(authorization)
+                .catch((err) => {
+                    throw Boom.unauthorized(err);
+                });
+            return h.authenticated({credentials: {user: payload['sub']}});
+        }
+
+    };
+});
+server.auth.strategy('google', 'google');
+server.auth.default('google');
+
 
 server.route({
     method: 'GET',
@@ -28,6 +57,17 @@ const init = async () => {
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
+
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+async function verify(token: string) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID
+    });
+    return ticket.getPayload();
+}
 
 process.on('unhandledRejection', (err) => {
 
